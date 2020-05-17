0d011ced3678b32d09274f2cb416e363c0e13118
raghadalquran <raghadalquran1@gmail.com>
2020-05-18 01:43:51 +0300

    done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
 let userInput = new Input();
-
 let newNotes = new Notes(userInput);
+
 newNotes.execute(userInput);
 newNotes.add(userInput);
