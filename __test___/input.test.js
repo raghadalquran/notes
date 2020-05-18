cf1c20bcc2accbb3331588cc6633395ee883cbac
raghadalquran <raghadalquran1@gmail.com>
2020-05-18 22:35:56 +0300

    working

--- __test___/input.test.js
+++ __test___/input.test.js
@@ -2,18 +2,32 @@
 
 const Input = require('../lib/input.js');
 
+let testSchema = {
+  id: { type: 'number', required: true },
+  name: { type: 'string', required: true },
+};
+
+let inValidObjectA = {};
+
+let validObject = {
+  id: 3,
+  name: 'sarah',
+};
+
+let validator = new Input(testSchema);
+
 describe('Input Module', () => {
-    it('if invalid objects , always return false', () => {
-        expect(validator.validate(inValidObjectA)).toBe(false);
-    });
-    it('if valid objects , return true', () => {
-        expect(validator.validate(validObject)).toBe(true);
-    });
-    it('.isString return false when not given a string', () => {
-        expect(validator.isString(3)).toBe(false);
-    });
-    it('.isString return true when given a string', () => {
-        expect(validator.isString('test')).toBe(true);
-        });
+  it('if invalid objects , always return false', () => {
+    expect(validator.validate(inValidObjectA)).toBe(false);
+  });
+  it('if valid objects , return true', () => {
+    expect(validator.validate(validObject)).toBe(true);
+  });
+  it('.isString return false when not given a string', () => {
+    expect(validator.isString(3)).toBe(false);
+  });
+  it('.isString return true when given a string', () => {
+    expect(validator.isString('test')).toBe(true);
+  });
 });
 
